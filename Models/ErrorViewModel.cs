cf441d1eba9a71c790b4167630476f5524a3e1db
liortanel <liortanel@gmail.com>
2025-05-05 13:50:08 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP04_Tanel_Dobrovitzky.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
