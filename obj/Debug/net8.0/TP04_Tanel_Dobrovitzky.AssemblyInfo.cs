41a2a4f2f7ac97084ea139296b80c4cec02bf277
liortanel <liortanel@gmail.com>
2025-05-19 15:38:16 -0300

    seventeenth commit

--- obj/Debug/net8.0/TP04_Tanel_Dobrovitzky.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP04_Tanel_Dobrovitzky.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP04_Tanel_Dobrovitzky")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+955ba2ae5bd67d38ce409dec5bbd409d32d481a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c79a1a8b35e797ecb20d5095ee754183ccf0f971")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP04_Tanel_Dobrovitzky")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP04_Tanel_Dobrovitzky")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
